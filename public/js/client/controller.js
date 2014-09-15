2f5da64e76b0c021545dfc0d6d7e9fd9c532e3b9
Ryan Stack <ryanstack@Ryans-MacBook-Pro-2.local>
2014-09-15 14:24:38 -0700

    adds marker pre-bootstrap

--- public/js/client/controller.js
+++ public/js/client/controller.js
@@ -1,14 +1,28 @@
 
 
 
+app.controller("UploadPrompter", function($scope) {
+
+  $scope.isTint = false;
+  $scope.isHolder = false;
+
+  $scope.makeTint = function() {
+    $scope.isTint = true;
+    var dropper = document.getElementById('holder').style.display = "block";
+    var options = document.getElementById('uploadOptions').style.display = "block";
+  }
+
+})
+
+
 app.controller("PercentFitUpload", function($scope) {
   $scope.process = function() {
-    console.log("tits")
     var DZ = Dropzone.forElement('.dropzone');
     DZ.processQueue();
   }
 
 })
+
 
 app.controller("PercentFitController", function($scope, $http) {
   $scope.picNumber = 0;
